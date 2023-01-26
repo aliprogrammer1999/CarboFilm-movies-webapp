c63c5cd0ce228f80a106b8f630bcd731a8c1bb83
ali-programmer-1999 <ali.programer.1999@gmail.com>
2023-01-26 10:43:46 -0800

    finish api

---
+++ pages/movies.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "@/components/Layout/Layout";
+
+function movies() {
+  return <Layout>movies</Layout>;
+}
+
+export default movies;
---
+++ pages/tv.js
@@ -0,0 +1,8 @@
+import Layout from "@/components/Layout/Layout";
+import React from "react";
+
+function tv() {
+  return <Layout>tv</Layout>;
+}
+
+export default tv;
