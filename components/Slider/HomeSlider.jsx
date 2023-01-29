345a366d59b0ca129e3a7582a59bed603c5c3747
ali-programmer-1999 <ali.programer.1999@gmail.com>
2023-01-29 10:38:46 -0800

    problam in swiper slider

--- components/Slider/HomeSlider.jsx
+++ components/Slider/HomeSlider.jsx
@@ -1,38 +1,50 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import React, { useRef, useState } from "react";
+import React, { useRef, useState , useEffect } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import style from "./Slider.module.css";
 
-import { Pagination, Navigation } from "swiper";
+import { Navigation, Thumbs } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
+import "swiper/css/free-mode";
 import "swiper/css/navigation";
+import "swiper/css/thumbs";
 
 export function HomeSlider({ poster }) {
-  console.log(poster);
-  return (
+
+  const [movie , setMovie] = useState([])
+    useEffect(()=>{
+       const moviesFilter = poster.filter(item=> item.media_type === 'movie')
+        setMovie(moviesFilter)
+    } , [])
+
+
+    return (
     <div>
       {" "}
       <Swiper
-        pagination={{
-          type: "progressbar",
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
+          style={{
+              "--swiper-navigation-color": "#fff",
+              "--swiper-pagination-color": "#fff",
+          }}
+          loop={true}
+          spaceBetween={10}
+          navigation={true}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Navigation, Thumbs]}
         className="mySwiper h-[95vh]"
       >
-        {poster.map((item) => (
+        {movie.map((item) => (
           <SwiperSlide key={item.id} className={style.postercss}>
             <img
               src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
             />
             <div className=" absolute left-[50%] translate-x-[-50%]">
-              <h1 className="text-4xl font-bold text-center mb-4">
-                {item.original_title}
+              <h1 className="text-6xl font-bold text-center mb-4">
+                {item.title}
               </h1>
               <p className="text-center">{item.overview}</p>
             </div>
